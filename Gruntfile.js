2580ba483ee2d8214ab9121957f066e11215ed44
Neilson <neilson@pettycache.info>
2016-07-01 12:56:45 +0800

    update grunt config with SVG tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,8 @@ module.exports = function(grunt) {
     //////////////////////////////
     // # image
     //////////////////////////////
+    // *
+    // minifying image
     imagemin: {
       build: {
         options: {
@@ -20,6 +22,52 @@ module.exports = function(grunt) {
           cwd: 'images/src/',
           src: ['**/*.{png,jpg,gif}'],
           dest: 'images/build/'
+        }]
+      }
+    },
+
+    // *
+    // minifying svg with svgo
+    svgmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'svgs/src/',
+          src: ['*.svg'],
+          dest: 'svgs/'
+        }]
+      },
+      options: {
+        plugins: [
+          { removeViewBox: false },
+          { removeEmptyAttrs: false }
+        ]
+      }
+    },
+
+    // *
+    // merge svgs in a folder
+    svgstore: {
+      dist: {
+        files: {
+          'svgs/build/svg-defs.svg': ['svgs/*.svg']
+        },
+      },
+      options: {
+        cleanup: true,
+        prefix: 'icon-'
+      }
+    },
+
+    // *
+    // build png fallbacks from svg
+    svg2png: {
+      dist: {
+        files: [{ 
+          flatten: true,
+          cwd: 'svgs/', 
+          src: ['*.svg'], 
+          dest: 'svgs/build/'
         }]
       }
     },
@@ -90,7 +138,7 @@ module.exports = function(grunt) {
     // concat javascript files
     concat: {
       dist: {
-        src: ['javascripts/libs/picturefill.min.js', 'javascripts/libs/eq.min.js', 'javascripts/libs/jquery.min.js', 'javascripts/libs/velocity.min.js', 'javascripts/libs/main.js'],
+        src: ['javascripts/libs/picturefill.min.js', 'svg4everybody.min.js', 'javascripts/libs/eq.min.js', 'javascripts/libs/jquery.min.js', 'javascripts/libs/velocity.min.js', 'javascripts/libs/main.js'],
         dest: 'javascripts/global.js',
       },
     },
@@ -119,14 +167,12 @@ module.exports = function(grunt) {
   ////////////////////////////////////////////////////////////////////////////////////////////////////
 
   //////////////////////////////
-  // # core
-  //////////////////////////////
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
-  //////////////////////////////
   // # media
   //////////////////////////////
   grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-svgmin');
+  grunt.loadNpmTasks('grunt-svgstore');
+  grunt.loadNpmTasks('grunt-svg2png');
 
   //////////////////////////////
   // # css
@@ -142,12 +188,24 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
+  //////////////////////////////
+  // # core
+  //////////////////////////////
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
 
 
   // # tasks
   ////////////////////////////////////////////////////////////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  //////////////////////////////
+  // # media
+  //////////////////////////////
+  grunt.registerTask('build-img', 'imagemin');
+  grunt.registerTask('build-svg', ['svgmin', 'svgstore', 'svg2png']);
+  grunt.registerTask('build-media', ['build-img', 'build-svg']);
 
   //////////////////////////////
   // # css
@@ -164,6 +222,6 @@ module.exports = function(grunt) {
   //////////////////////////////
   // # default
   //////////////////////////////
-  grunt.registerTask('default', ['build-css', 'build-js']);
+  grunt.registerTask('default', ['build-media', 'build-css', 'build-js']);
 
 };
