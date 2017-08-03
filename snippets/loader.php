aa2790be02169b5463934a5ca02267a575f75cce
Neilson <neilson@strato-sphere.com>
2017-08-03 11:26:56 +0800

    [1.1.47] add JS conditional classes for select properties
    - remove loader component module
    - add js enhanced classes for loader component

--- snippets/loader.php
+++ snippets/loader.php
@@ -1,7 +1,8 @@
 <!-- loader -->
 <div class="
   pos-fixed z-2 top-0 left-0
-  d-table w-100 h-100
+  d-none vis-hidden w-100 h-100
+    js-d-table js-vis-visible
   bg-c-white
   loader">
   <!-- loader graphic -->
